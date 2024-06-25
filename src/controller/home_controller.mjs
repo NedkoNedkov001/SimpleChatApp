d6095e194a0ac1732ed2a4cee78e8cb2c1340e7d
nedkonedkov001 <nedkonedkov001@gmail.com>
2024-06-25 22:20:11 +0300

    Added chat page

--- src/controller/home_controller.mjs
+++ src/controller/home_controller.mjs
@@ -1,6 +1,11 @@
-export const controller = {}
+export const controller = {};
 
 controller.home = async (req, res) => {
-    const data = { title: "SimpleChatApp", page: '../home/index' };
-    return res.render('shared/layout', data);
-  }
+  const data = { title: "SimpleChatApp", page: "../home/index" };
+  return res.render("shared/layout", data);
+};
+
+controller.chat = async (req, res) => {
+  const data = { title: "Chats", page: "../home/chat" };
+  return res.render("shared/layout", data);
+};
